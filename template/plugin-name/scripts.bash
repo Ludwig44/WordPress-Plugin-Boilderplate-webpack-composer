5a19a921f2f8101a9ec45c2a80eb4d6b3528da8a
Ludwig <ludwig@webdeclic.com>
2023-12-28 17:30:35 +0100

    modification on export to zip script

--- template/plugin-name/scripts.bash
+++ template/plugin-name/scripts.bash
@@ -17,7 +17,7 @@ then
     rm -rf $plugin_name.zip;
     exclude_files="--exclude=.* --exclude=.DS_Store --exclude=__MACOSX --exclude=scripts.bash --exclude=$plugin_name.zip --exclude=public/assets/src/* --exclude=*package-lock.json --exclude=*package.json --exclude=*README.md --exclude=*webpack.config.js --exclude=*node_modules/*";
     echo "Exporting to ZIP";
-    zip -rq $plugin_name.zip $plugin_name $exclude_files;
+    zip -rq $plugin_name.zip ./* $exclude_files;
     
      # Open the folder
     open "$current_dir";
